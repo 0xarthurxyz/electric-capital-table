9c49ec96edeb2b71de4b5f07f0e1321ef1ef8590
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-05-08 22:52:12 +0100

    feat(data grid): adds github fetch logic, base64 decoding and JSON formatting (but react data grid is not displaying rows correctly)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
 import "./styles.css";
+import "react-data-grid/lib/styles.css";
 
-export default function App() {
+
+// async function format(filePath) {
+//   try {
+//       const [data, setData] = useState([]);
+
+//       useEffect(() => {
+//         async function fetchData() {
+//           const response = await fetch(filePath);
+//           const text = await response.text();
+//           const parsedData = parse(text);
+//           setData(parsedData);
+//         }
+//         fetchData();
+//       }, []);
+
+      
+//       var formattedData = [];
+//       for (let index = 0; index < data.repo.length; index++) {
+//           var json = {};
+//           json["id"] = index;
+//           json["title"] = data.title;
+//           json["repo"] = data.repo[index].url
+//           formattedData.push(json);
+//       }
+//       console.log(formattedData);
+//       return formattedData;
+//   } catch (error) {
+//       console.log(error);
+//   }
+// }
+
+// const columns = [
+//   {
+//     name: "Title",
+//     selector: (row) => row.title,
+//     sortable: true
+//   },
+//   {
+//     name: "Repo",
+//     selector: (row) => row.repo,
+//     sortable: true
+//   }
+// ];
+
+// const rows = format("https://github.com/electric-capital/crypto-ecosystems/blob/master/data/ecosystems/c/celo.toml")
+
+// export default function App() {
+//   return <DataGrid columns={columns} rows={rows} />;
+// }
+
+import React from "react";
+import CeloDataGrid from "./DataGrid";
+
+function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <h1>Celo Data</h1>
+      <CeloDataGrid />
     </div>
   );
 }
+
+export default App;
---
+++ src/DataGrid.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import ReactDataGrid from "react-data-grid";
+import * as toml from 'toml';
+
+
+function decodeBase64(encodedData) {
+    if (typeof window === 'undefined') {
+      // Node.js environment
+      const buffer = Buffer.from(encodedData, 'base64');
+    //   console.log(`buffer.toString('utf-8') + ${buffer.toString('utf-8')}`)
+      return buffer.toString('utf-8');
+    } else {
+      // Browser environment
+      const decoder = new TextDecoder();
+      const data = Uint8Array.from(atob(encodedData), c => c.charCodeAt(0));
+    //   console.log(`decoder.decode(data) + ${decoder.decode(data)}`)
+      return decoder.decode(data);
+    }
+  }
+
+    async function getDataFromgithub (owner, repo, path) { 
+    // A function to fetch files from github using the api 
+    
+    let data = await fetch (
+        `https://api.github.com/repos/${owner}/${repo}/contents/${path}`
+      )
+        .then (d => d.json ())
+        .then (d =>
+          fetch (
+            `https://api.github.com/repos/${owner}/${repo}/git/blobs/${d.sha}`
+          )
+        )
+        .then (d => d.json ())
+        .then (d => {
+          console.log(`d.content + ${d.content}`)
+            return decodeBase64(d.content);
+        });
+        return data;
+}
+
+function CeloDataGrid() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+    //   const response = await fetch(
+    //     "https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/c/celo.toml"
+    //   );
+      const parsedString = await getDataFromgithub("electric-capital", "crypto-ecosystems", "data/ecosystems/c/celo.toml");
+      console.log(`parsedString + ${parsedString}`);
+    //   const text = await response.text();
+    //   const parsedData = parse(text);
+        const parsedData = toml.parse(parsedString);
+        console.log(parsedData, JSON.stringify(parsedData));
+
+      var formattedData = [];
+      for (let index = 0; index < parsedData.repo.length; index++) {
+          var json = {};
+          json["key"] = index;
+        //   json["id"] = parsedData.repo[index].url;
+          json["title"] = parsedData.title;
+          json["repo"] = parsedData.repo[index].url
+          formattedData.push(json);
+      }
+    //   console.log(formattedData, JSON.stringify(formattedData));
+      console.log(parsedData.repo[0].url);
+      setData(formattedData);
+    }
+    fetchData();
+  }, []);
+
+  const columns = [
+    {
+      name: "Key",
+      selector: (row) => row.key,
+      sortable: true
+    },
+    {
+      name: "Title",
+      selector: (row) => row.title,
+      sortable: true
+    },
+    {
+      name: "Repo",
+      selector: (row) => row.repo,
+      sortable: true
+    }
+  ];
+
+  if (!data) {
+    return <div>Loading...</div>; // render loading message while data is being fetched
+  }
+
+  console.log(data, JSON.stringify(data));
+  return <ReactDataGrid columns={columns} rows={data} rowKey="id" />;
+// return (
+//     <ReactDataGrid
+//       columns={columns}
+//       rows={data.map((row) => ({ ...row, key: row.id }))}
+//     />
+//   );
+}
+
+export default CeloDataGrid;
